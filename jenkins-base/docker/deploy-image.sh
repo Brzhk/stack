103bcd0367f2214b75386dd202cd3e7df39dc4b3
Brzhk <berzehk@gmail.com>
2017-07-10 09:03:06 +0200

    fixes..

--- jenkins-base/docker/deploy-image.sh
+++ jenkins-base/docker/deploy-image.sh
@@ -6,9 +6,7 @@ image_name=$2
 
 # If the image doesn't exists, we build it and push it to the repository.
 if [[ "$(docker images -q ${repository_url} 2> /dev/null)" == "" ]]; then
-  cd docker
   docker build -t ${image_name} .
   docker tag ${image_name}:latest ${repository_url}:latest
   docker push ${repository_url}:latest
-  cd -
 fi
