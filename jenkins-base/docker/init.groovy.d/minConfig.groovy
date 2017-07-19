d31b71369e65d1e4e9af9cbfe04b05f1a371178f
Matthieu Bertin <berzehk@gmail.com>
2017-07-19 21:46:48 +0200

    better password management

--- jenkins-base/docker/init.groovy.d/minConfig.groovy
+++ jenkins-base/docker/init.groovy.d/minConfig.groovy
@@ -1,9 +1,12 @@
 import com.cloudbees.jenkins.plugins.amazonecs.ECSCloud
 import com.cloudbees.jenkins.plugins.amazonecs.ECSTaskTemplate
+import hudson.BulkChange
+import hudson.FilePath
 import hudson.model.Node
 import hudson.plugins.git.GitSCM
 import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
 import hudson.security.HudsonPrivateSecurityRealm
+import hudson.security.SecurityRealm
 import hudson.security.csrf.DefaultCrumbIssuer
 import jenkins.CLI
 import jenkins.model.Jenkins
@@ -16,7 +19,6 @@ import org.testng.collections.Lists
 import java.util.logging.Logger
 
 def logger = Logger.getLogger("")
-def instance = Jenkins.getInstance()
 
 int defined_slaveport = 50000
 
@@ -51,79 +53,116 @@ def emptyCreds = ""
 def regionName = "eu-west-1"
 def slaveTimeoutInSeconds = 900
 
-HudsonPrivateSecurityRealm hudsonRealm = new HudsonPrivateSecurityRealm(false)
-FullControlOnceLoggedInAuthorizationStrategy strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.allowAnonymousRead = false
-def users = hudsonRealm.getAllUsers()
-if (!users || users.empty) {
-    logger.info "No Users"
 
-    logger.info"--> creating local user 'Brzhk'"
-    hudsonRealm.createAccount('Brzhk', 'brzhkbrzhk')
-
-    logger.info "--> setting full control once logger authorization strategy"
-
-    instance.setAuthorizationStrategy(strategy)
-
-    logger.info "--> disabling CLI remote access"
-    CLI.get().setEnabled(false)
-
-    logger.info "--> creating crumb issuer"
-    DefaultCrumbIssuer defaultCrumbIssuer = new DefaultCrumbIssuer(true)
-    instance.crumbIssuer = defaultCrumbIssuer
-    instance.setSecurityRealm(hudsonRealm)
-
-    logger.info "--> enabling slave access control mechanism"
-    instance.getInjector().getInstance(AdminWhitelistRule.class)
-            .setMasterKillSwitch(false)
-
-    logger.info "--> setting slave port"
-    instance.setSlaveAgentPort(defined_slaveport)
-
-    def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-
-    jenkinsLocationConfiguration.setAdminAddress("Brzhk <berzehk@gmail.com>")
-    jenkinsLocationConfiguration.setUrl("https://jenkins.brzhk.wtf/")
-    jenkinsLocationConfiguration.save()
-
-    instance.setLabelString("master")
-    instance.setMode(Node.Mode.EXCLUSIVE)
-    instance.setScmCheckoutRetryCount(2)
-    instance.setSystemMessage("-- Press START --")
-
-    GitSCM.DescriptorImpl gitDesc = Jenkins.instance.getExtensionList(GitSCM.DescriptorImpl.class)[0]
-    gitDesc.globalConfigEmail = "berzehk@gmail.com"
-    gitDesc.globalConfigName = "Brzhk"
-    gitDesc.createAccountBasedOnEmail = false
-    gitDesc.save()
-
-    GlobalConfig pipelineCfg = GlobalConfig.get()
-    pipelineCfg.dockerLabel = dindTaskLabel
-    pipelineCfg.setRegistry(new DockerRegistryEndpoint(null, null))
-    pipelineCfg.save()
-
-    logger.info "--> configuring java slave agent"
-    List<ECSTaskTemplate.LogDriverOption> logDriverOptions = Collections.singletonList(new ECSTaskTemplate.LogDriverOption("tag", taskTemplateName))
-    List<ECSTaskTemplate.EnvironmentEntry> environments = Collections.EMPTY_LIST
-    List<ECSTaskTemplate.ExtraHostEntry> extraHosts = Collections.EMPTY_LIST
-    List<ECSTaskTemplate.MountPointEntry> mountPoints = Collections.EMPTY_LIST
-    ECSTaskTemplate taskTemplate = new ECSTaskTemplate(taskTemplateName, taskLabel, taskImage, taskRemoteFSRoot, taskMemory, taskMemoryReservation, taskCpu, privileged, logDriverOptions, environments, extraHosts, mountPoints)
-    taskTemplate.setLogDriver(tasklogDriver)
-
-    logger.info "--> configuring dind slave agent"
-    List<ECSTaskTemplate.LogDriverOption> dindLogDriverOptions = Collections.singletonList(new ECSTaskTemplate.LogDriverOption("tag", taskTemplateName))
-    List<ECSTaskTemplate.EnvironmentEntry> dindEnvironments = Collections.EMPTY_LIST
-    List<ECSTaskTemplate.ExtraHostEntry> dindExtraHosts = Collections.EMPTY_LIST
-    List<ECSTaskTemplate.MountPointEntry> dindMountPoints = Collections.singletonList(new ECSTaskTemplate.MountPointEntry('varlibdocker','','/var/lib/docker', false))
-    ECSTaskTemplate dindTaskTemplate = new ECSTaskTemplate(dindTaskTemplateName, dindTaskLabel, dindTaskImage, dindTaskRemoteFSRoot, dindTaskMemory, dindTaskMemoryReservation, dindTaskCpu, dindPrivileged, dindLogDriverOptions, dindEnvironments, dindExtraHosts, dindMountPoints)
-    dindTaskTemplate.setLogDriver(dindTasklogDriver)
+final String ADMIN_USERNAME = 'Brzhk'
+Jenkins instance = Jenkins.getInstance()
+final FilePath ADMIN_PASSWORD_FILE = instance.getRootPath().child('secrets/initialAdminPassword')
+SecurityRealm securityRealm = instance.getSecurityRealm() ? instance.getSecurityRealm() : new HudsonPrivateSecurityRealm(false)
 
 
-    ECSCloud ecsCloud = new ECSCloud(cloudName, Lists.newArrayList(taskTemplate, dindTaskTemplate), emptyCreds, cloudClusterArn, regionName, jenkinsUrl, slaveTimeoutInSeconds)
-    ecsCloud.tunnel = tunnel
-    instance.clouds.add(ecsCloud)
+if (securityRealm instanceof HudsonPrivateSecurityRealm
+        && securityRealm.getAllUsers().isEmpty()) {
+
+    HudsonPrivateSecurityRealm defaultSecurityRealm = securityRealm
+
+    logger.info "--> creating ${ADMIN_USERNAME} local user"
+    String generatedPassword = UUID.randomUUID().toString().replace('-', '').toLowerCase(Locale.ENGLISH)
+    new BulkChange(instance).withClosable {
+
+        defaultSecurityRealm.createAccount(ADMIN_USERNAME, generatedPassword)
+//        User admin = defaultSecurityRealm.createAccount(ADMIN_USERNAME, generatedPassword)
+//        assert ExtensionList.lookup(PermissionAdder.class).any {
+//            it.add(instance.getAuthorizationStrategy(), admin, Jenkins.ADMINISTER)
+//        }: "Cannot give the ADMINISTER authority to the ${ADMIN_USERNAME} user"
+
+        ADMIN_PASSWORD_FILE.touch(System.currentTimeMillis())
+        ADMIN_PASSWORD_FILE.chmod(0640)
+        ADMIN_PASSWORD_FILE.write(generatedPassword + System.lineSeparator(), 'UTF-8')
+
+        logger.info "--> setting full control once logger authorization strategy"
+        FullControlOnceLoggedInAuthorizationStrategy strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+        strategy.allowAnonymousRead = false
+        instance.setAuthorizationStrategy(strategy)
+
+        logger.info "--> disabling CLI remote access"
+        CLI.get().setEnabled(false)
+
+        logger.info "--> creating crumb issuer"
+        DefaultCrumbIssuer defaultCrumbIssuer = new DefaultCrumbIssuer(true)
+        instance.crumbIssuer = defaultCrumbIssuer
+        instance.setSecurityRealm(defaultSecurityRealm)
+
+        logger.info "--> enabling slave access control mechanism"
+        instance.getInjector().getInstance(AdminWhitelistRule.class)
+                .setMasterKillSwitch(false)
+
+        logger.info "--> setting slave port"
+        instance.setSlaveAgentPort(defined_slaveport)
+
+        def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+
+        jenkinsLocationConfiguration.setAdminAddress("Brzhk <berzehk@gmail.com>")
+        jenkinsLocationConfiguration.setUrl("https://jenkins.brzhk.wtf/")
+        jenkinsLocationConfiguration.save()
+
+        instance.setLabelString("master")
+        instance.setMode(Node.Mode.EXCLUSIVE)
+        instance.setScmCheckoutRetryCount(2)
+        instance.setSystemMessage("-- Press START --")
+
+        GitSCM.DescriptorImpl gitDesc = Jenkins.instance.getExtensionList(GitSCM.DescriptorImpl.class)[0]
+        gitDesc.globalConfigEmail = "berzehk@gmail.com"
+        gitDesc.globalConfigName = "Brzhk"
+        gitDesc.createAccountBasedOnEmail = false
+        gitDesc.save()
+
+        GlobalConfig pipelineCfg = GlobalConfig.get()
+        pipelineCfg.dockerLabel = dindTaskLabel
+        pipelineCfg.setRegistry(new DockerRegistryEndpoint(null, null))
+        pipelineCfg.save()
+
+        logger.info "--> configuring java slave agent"
+        List<ECSTaskTemplate.LogDriverOption> logDriverOptions = Collections.singletonList(new ECSTaskTemplate.LogDriverOption("tag", taskTemplateName))
+        List<ECSTaskTemplate.EnvironmentEntry> environments = Collections.EMPTY_LIST
+        List<ECSTaskTemplate.ExtraHostEntry> extraHosts = Collections.EMPTY_LIST
+        List<ECSTaskTemplate.MountPointEntry> mountPoints = Collections.EMPTY_LIST
+        ECSTaskTemplate taskTemplate = new ECSTaskTemplate(taskTemplateName, taskLabel, taskImage, taskRemoteFSRoot, taskMemory, taskMemoryReservation, taskCpu, privileged, logDriverOptions, environments, extraHosts, mountPoints)
+        taskTemplate.setLogDriver(tasklogDriver)
+
+        logger.info "--> configuring dind slave agent"
+        List<ECSTaskTemplate.LogDriverOption> dindLogDriverOptions = Collections.singletonList(new ECSTaskTemplate.LogDriverOption("tag", taskTemplateName))
+        List<ECSTaskTemplate.EnvironmentEntry> dindEnvironments = Collections.EMPTY_LIST
+        List<ECSTaskTemplate.ExtraHostEntry> dindExtraHosts = Collections.EMPTY_LIST
+        List<ECSTaskTemplate.MountPointEntry> dindMountPoints = Collections.singletonList(new ECSTaskTemplate.MountPointEntry('varlibdocker', '', '/var/lib/docker', false))
+        ECSTaskTemplate dindTaskTemplate = new ECSTaskTemplate(dindTaskTemplateName, dindTaskLabel, dindTaskImage, dindTaskRemoteFSRoot, dindTaskMemory, dindTaskMemoryReservation, dindTaskCpu, dindPrivileged, dindLogDriverOptions, dindEnvironments, dindExtraHosts, dindMountPoints)
+        dindTaskTemplate.setLogDriver(dindTasklogDriver)
 
 
-    instance.save()
+        ECSCloud ecsCloud = new ECSCloud(cloudName, Lists.newArrayList(taskTemplate, dindTaskTemplate), emptyCreds, cloudClusterArn, regionName, jenkinsUrl, slaveTimeoutInSeconds)
+        ecsCloud.tunnel = tunnel
+        instance.clouds.add(ecsCloud)
+
+        it.commit()
+
+        if (ADMIN_PASSWORD_FILE.exists()) {
+            String setupKey = ADMIN_PASSWORD_FILE.readToString().trim()
+            logger.info("""
+                *************************************************************
+                *************************************************************
+                *************************************************************
+                    
+                Jenkins initial setup is required. An admin user has been 
+                created and a password generated. Please use the following 
+                password to proceed to installation:
+                    
+                    ${setupKey} 
+                    
+                This may also be found at: ${ADMIN_PASSWORD_FILE.getRemote()}
+                    
+                *************************************************************
+                *************************************************************
+                *************************************************************""")
+        }
+    }
 } else
     logger.info "Admin found"
